caa3f5dbe4bebc4fc51ccad5c11874545a46a2d9
iikim <iikim2511@gmail.com>
2021-09-05 21:18:23 -0400

    js: add: reset btn

---
+++ script.js
@@ -0,0 +1,25 @@
+(function() {
+        
+    const stage = document.querySelector('.stage');
+    
+    const resetBtn = document.querySelector('.reset');
+
+
+    // js 10. click and remove
+    
+    stage.addEventListener('click', clickHandler);
+
+    function clickHandler(e) {
+        if (e.target.classList.contains('poke')) {
+            stage.removeChild(e.target);
+        }
+    }
+
+    //js  20. reset
+
+    resetBtn.addEventListener("click",resetFun);
+
+    function resetFun() {
+        location.reload();           
+    }            
+})();
